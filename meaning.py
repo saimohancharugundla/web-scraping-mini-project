9290069d8b442316383097a19b67f212345d4d8e
saimohancharugundla <55023307+saimohancharugundla@users.noreply.github.com>
2019-11-08 23:04:11 +0530

    Add files via upload
    This code glances you to find the meaning,phonetic transcription,parts of speech and usage of the given word ,scraped from online cambridge dictionary.
    Modules used:
    1.requests
    2.beautiful soup
    3.termcolor

---
+++ meaning.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup as bs
+from termcolor import colored
+i="y"
+while i=='y':
+	try:
+		word=input("Word:")
+		headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
+		url=requests.get("https://dictionary.cambridge.org/dictionary/english/{}".format(word),headers=headers)
+		total_content=url.content
+		soup=bs(total_content,"html.parser")
+		word=soup.find("span",attrs={"class":"hw dhw"}).text
+		pos=soup.find("div",attrs={"class":"posgram dpos-g hdib lmr-5"}).text
+		phonetic=soup.find("span",attrs={"class":"ipa dipa lpr-2 lpl-1"}).text
+		lis=[]
+		usage=[]
+		meaning = soup.find("div",attrs={"class":"def ddef_d db"}).text
+		print("\n")
+		print(colored("WORD:","white"),word)
+		print(colored("PARTS OF SPEECH:","white"),pos)
+		print(colored("PHONETIC DESCRIPTION:","white"),phonetic+"\n")
+	
+		print(colored("Meaning:","white"))
+		print(meaning)
+		print("\n")
+		print(colored('Lets see its usage!!\n\n','blue'))
+		
+		for j in soup.find_all("div",attrs={"class":"def-body ddef_b"}):
+			use=(j.find("div",attrs={"class":"examp dexamp"})).text.replace("\n"," ")
+			usage.append(use)
+		for k in usage:
+			print(k)
+		print("\n\n")
+	except AttributeError:
+		print("Sorry!I dont find the word")
+	except requests.exceptions.ConnectionError:
+		print("It seems your connection is too bad or no connection!!")
+	i=input("Do you want another [y/n]:")	
